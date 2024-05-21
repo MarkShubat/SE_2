dda3d29353114754bd0cd9c83fcd4b770e9f72df
Марк Шубат <71442207+MarkShubat@users.noreply.github.com>
2024-05-21 22:34:57 +0500

    Update use_model.py

--- use_model.py
+++ use_model.py
@@ -1,8 +1,8 @@
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-from io import BytesIO
 from google.colab import drive
+
 
 def read_and_preprocess_image(image_path):
     img = Image.open(image_path)
@@ -12,6 +12,7 @@ def read_and_preprocess_image(image_path):
     img_array = np.expand_dims(img_array, axis=0)
     return tf.convert_to_tensor(img_array, dtype=tf.float32)
 
+
 def main():
     drive.mount('/content/drive')
     model_path = "/content/drive/MyDrive/Models/proj.h5"
@@ -20,6 +21,7 @@ def main():
     tensor = read_and_preprocess_image(image_path)
     prediction = model.predict(tensor)
     print("Prediction:", prediction)
-    
+
+
 if __name__ == "main":
     main()
