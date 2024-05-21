e87a4fa001897ec8dbfd79570a1d7bde56358904
Roflanlico <92446877+Roflanlico@users.noreply.github.com>
2024-05-21 23:14:46 +0500

    Update Model_learn.py

--- Model_learn.py
+++ Model_learn.py
@@ -2,24 +2,15 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
-import shutil
 from PIL import Image
 import pandas as pd
-import zipfile
-from zipfile import ZipFile
-from io import BytesIO
-from PIL import Image
 # from google.colab import drive
 import glob
 import os
-from re import X
-import pickle
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from sklearn.model_selection import train_test_split
-from keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping
 
 
 for dirname, _, filenames in os.walk('/content'):
@@ -27,26 +18,24 @@ for dirname, _, filenames in os.walk('/content'):
         print(os.path.join(dirname, filename))
 
 
-classes = [ 'Amarant', 'Cabbage', 'Watercress']
+classes = ['Amarant', 'Cabbage', 'Watercress']
 directory_to_extract_to = '/content/drive/My Drive/extracted_images'
-image_df = pd.DataFrame(columns = ['X','y'])
+image_df = pd.DataFrame(columns=['X', 'y'])
 images_set = []
 
-
 for e in classes:
-  dir = directory_to_extract_to  + f'/{e}'
-  # Получите список всех файлов изображений в директории
-  images_path = glob.glob(dir + '/*.jpg') # Или другой формат изображения
-  # Считайте изображения
-  images = [Image.open(img_path) for img_path in images_path]
+    dir = directory_to_extract_to + f'/{e}'
+    # Получите список всех файлов изображений в директории
+    images_path = glob.glob(dir + '/*.jpg') # Или другой формат изображения
+    # Считайте изображения
+    images = [Image.open(img_path) for img_path in images_path]
 
-  images_set.append(images)
-
+    images_set.append(images)
 
   for dirname, _, filenames in os.walk('/content'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-        # Проходим по всем файлам в архиве
+      for filename in filenames:
+          print(os.path.join(dirname, filename))
+          # Проходим по всем файлам в архиве
 
 
 def crop_img(image_CV2, size_x, size_y):
@@ -81,9 +70,7 @@ def slice_image(image_PIL, chunk_size, tmp_set):
             # region.save(f'chunk_{x}_{y}.png')
             tmp_set.append(region)
 
-
 chunk_size = 224
-
 
 images_set_2 = []
 for img_set in images_set:
@@ -95,7 +82,6 @@ for img_set in images_set:
     tmp_set.append(image)
   images_set_2.append(tmp_set)
 images_set = []
-
 
 images_set_3 = []
 for img_set in images_set_2:
@@ -110,10 +96,8 @@ for img_set in images_set_2:
   images_set_3.append(tmp_set[0: 512])
 images_set_2 = []
 
-
 for akd in images_set_3:
   print(len(akd))
-
 
 images_set_4 = []
 for img_set in images_set_3:
@@ -124,7 +108,6 @@ for img_set in images_set_3:
   images_set_4.append(tmp_set)
 images_set_3 = []
 
-
 list_x = []
 list_y = []
 for i in range(3):
@@ -132,24 +115,19 @@ for i in range(3):
    list_y = list_y + [i for j in range(512)]
 print(list_y)
 
-
 final_df = pd.DataFrame({'X': list_x, 'y': list_y})
-
 
 imgs_dir = '/content/images'
 os.makedirs(imgs_dir, exist_ok=True)
 for dir in classes:
   os.makedirs(f'{imgs_dir}/{dir}', exist_ok=True)
 
-
 for i in range(3):
   for j in range(len(images_set_4[i])):
     cv2.imwrite(f'/content/images/{classes[i]}/{j}.jpg', images_set_4[i][j])
 
-
 # Каталог с набором данных
 data_dir = '/content/images'
-
 
 early_stop = EarlyStopping(
     monitor='val_loss',  # Метрика для отслеживания
@@ -159,14 +137,11 @@ early_stop = EarlyStopping(
     restore_best_weights=True
 )
 
-
 classes = [ 'Amarant', 'Cabbage', 'Watercress']
-
 
 datagen = ImageDataGenerator(
     rescale=1./255,
     validation_split=0.2 )
-
 
 train_generator = datagen.flow_from_directory(
     data_dir,
@@ -175,7 +150,6 @@ train_generator = datagen.flow_from_directory(
     class_mode='categorical',  # Режим классификации
     classes = classes,
     subset='training')
-
 
 test_data_generator = datagen.flow_from_directory(
     data_dir,
@@ -186,20 +160,16 @@ test_data_generator = datagen.flow_from_directory(
     subset='validation'  # Определение тестовой выборки
 )
 
-
 class_indices = train_generator.class_indices
 print(class_indices)
 
-
 class_indices = test_data_generator.class_indices
 print(class_indices)
-
 
 file_paths = train_generator.filepaths
 for path in file_paths:
     if '.ipynb_checkpoints' in path:
        print(path)
-
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
@@ -211,13 +181,10 @@ model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
 model.add(Dense(512, activation='relu'))
 model.add(Dense(3, activation='softmax')) #sigmoid
-
 model.summary()
-
 model.compile(optimizer='adam',
           loss='categorical_crossentropy',
           metrics=['accuracy'])
-
 
 history = model.fit(
     train_generator,
@@ -226,9 +193,7 @@ history = model.fit(
     callbacks=[early_stop]
     )
 
-
 model.evaluate(test_data_generator, verbose = 1)
-
 
 plt.plot(history.history['accuracy'],
          label='Доля правильных ответов на обучающем наборе')
@@ -238,6 +203,5 @@ plt.xlabel('Эпоха обучения')
 plt.ylabel('Доля правильных ответов')
 plt.legend()
 plt.show()
-
 
 model.save('proj.h5')
